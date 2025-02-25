5aa3c4c85ec025541d2c97b2cce34a77bbe60a86
Benjamin Rosario <benjaminfl119@gmail.com>
2025-02-25 10:06:13 -0400

    refactor database connection logic and update output directory in TypeScript configuration

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,16 +7,9 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 // Cargar variables de entorno
 config()
-
-// Inicializar Oracle Client si está configurado
-if (process.env.ORACLE_INSTANT_CLIENT_ROUTE) {
-  initOracleClient({ libDir: process.env.ORACLE_INSTANT_CLIENT_ROUTE })
-}
-
 // Realiza la conexión a la base de datos y crea la aplicación Express en una función asíncrona autoejecutable.
 // Nota: Este ejemplo asume que la conexión se puede reutilizar en llamadas subsiguientes.
 let app: ReturnType<typeof createExpressApp>
-
 ;(async () => {
   await connectDatabase()
   app = createExpressApp()
